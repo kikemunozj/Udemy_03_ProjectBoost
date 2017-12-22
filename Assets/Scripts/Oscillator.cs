86cfc5f29e85086b9d4982941baeb7cbbf4b1764
Enrique <kikemunozj@gmail.com>
2017-12-22 16:16:24 +0000

    no message

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+
+[DisallowMultipleComponent]
+
+public class Oscillator : MonoBehaviour {
+    
+    [SerializeField] Vector3 movementVector = new Vector3(10f,10f,10f);
+    [SerializeField] float period = 2f;
+
+    [Range(0,1)] [SerializeField] float movementFactor;
+
+    Vector3 startingPos;
+
+	void Start () 
+    {
+        startingPos = transform.position;
+	}
+	
+
+	void Update () 
+    {
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2f;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = (float)(rawSinWave / 2f + 0.5);
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+	}
+}
